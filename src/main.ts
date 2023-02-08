4664c4e2f9e0bc689af698f62593bc5c8728852f
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-02-08 19:47:36 +0300

    refactor: clear comments in matin.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { ValidationPipe } from './pipes/validation.pipe';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(new ValidationPipe());
   app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
   await app.listen(PORT);
 }
