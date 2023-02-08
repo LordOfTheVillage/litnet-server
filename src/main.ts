4e514ee41bb12d30300874f6f97f440568daa9c5
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-02-08 21:50:09 +0300

    refactor: change port parsing into integer

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 5000;
+  const PORT = parseInt(`${process.env.PORT}`) || 5000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
   await app.listen(PORT);
