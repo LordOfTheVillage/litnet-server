c919f3d468573cbe3894fadd42117cda96dc1e60
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-02-23 22:49:35 +0300

    feat; update user model

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -65,7 +65,7 @@ export class User extends Model<User, UserCreationAttrs> {
   ratings: Rating[];
 
   @HasMany(() => Comment)
-  comments: Comment[];
+  bookComments: Comment[];
 
   @HasMany(() => Bookmark)
   bookmarks: Bookmark[];
