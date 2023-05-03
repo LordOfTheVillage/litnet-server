733ad1df9689191cc39b561c1d01880f126fec39
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-05-03 19:27:58 +0300

    fix: repair config

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: null,
+    database: 'database_development',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    dialect: 'postgres',
+  },
+};
